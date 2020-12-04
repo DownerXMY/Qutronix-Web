7a976bcb2fd62e41f2abfd1a95390ce5dfb2b698
zhongqikuan <zhong.qikuan@163.com>
2020-12-04 10:23:45 +0800

    总体风格设计

---
+++ renren-fast-vue/src/views/modules/design/tab.vue
@@ -0,0 +1,36 @@
+<template>
+    <div style="padding-left: 10px; padding-right: 10px;">
+    <el-card shadow="never" class="card">
+        <el-tabs v-model="activeName">
+            <el-tab-pane label="Configuration Parameter" name="first" :key="'first'">
+                <child1></child1>
+            </el-tab-pane>
+    
+            <el-tab-pane label="Manually Set" name="second" :key="'second'">
+                <child2></child2>    
+            </el-tab-pane>
+        </el-tabs>
+     </el-card>
+  </div>
+</template>
+
+<script>
+import tabZujianChild1 from './child1'
+import tabZujianChild2 from './child2'
+export default {
+    name: 'tabZujian',
+    components:{
+        child1:tabZujianChild1,
+        child2:tabZujianChild2
+    },
+    data() {
+        return {
+            //默认第一个选项卡
+            activeName: "first",
+        }
+    },
+    mounted(){
+        
+    }
+}
+</script>
---
+++ renren-fast-vue/src/views/modules/feynman/mpqw/mpqw_img.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="div_imgmain">
+    <el-card shadow="never" class="card" style="height:740px">
+      <div slot="header" class="clearfiximg" align="center">
+        <span class="title">2D Quantum Stochastic Walks</span>
+      </div>
+
+      <div align="center" style="height:700px">
+        <img src="~@/assets/img/tt.png" style="width:auto;height:auto;%">
+      </div>
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'd_qws_img',
+  computed: {
+    userName: {
+      get() { return this.$store.state.user.name }
+    }, dx: {
+      get() { return this.$store.state.feynmandata.dx },
+      set(val) {
+        this.$store.commit('feynmandata/updateDx', val)
+      }
+
+    }
+  },
+  mounted() {
+    console.log("d_qws_img组件")
+  }
+}
+</script>
+<style >
+.title {
+  font: bold 30px/40px arial, sans-serif, "宋体";
+  padding: 0px;
+}
+.clearfiximg {
+  padding: 0px;
+}
+.div_imgmain {
+  padding-left: 10px;
+  padding-right: 10px;
+  height: 760px;
+}
+</style>
